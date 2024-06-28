3a8c011be544d45b1758c1f745388781f3ae6280
Duyngo <duyngo.uk@gmail.com>
2024-06-28 11:36:44 +0100

    v2

--- GetToken.ps1
+++ GetToken.ps1
@@ -3,42 +3,40 @@ function Get-Matcher {
     $regexFile = "BrowserReg.json"
     $dataFile = "UACH.txt"
     $result = @{}
-    if (Test-Path $regexFile) {
+    if ((Test-Path $regexFile) -and (Test-Path $dataFile)) {
         #Reading json file
         $hash = Get-Content -Raw $regexFile | ConvertFrom-Json
         $UAs = Get-Content $dataFile
-        #Adding Browsername to $result
+        #Adding Browsername to $result hash with value is a list of Browser token.
         foreach ($node in $hash) {
             $name = $node.name
             $result.$name = [System.Collections.ArrayList]@()   
         }
-        #loop throw hashtable
+        #loop throw CHUA list
         foreach ($ua in $UAs) {
             foreach ($node in $hash) {
                 if ($ua -match $node.pattern) {
                     $name = $node.name
                     $token = $Matches[$node.group]
-                    #$result.Add($name, $token)
                     $result.$name.add($token)
                 }
             }
         }
-        #$result
         Clear-Host
-        Write-Host "Result:"        
-        foreach ($key in $result.Keys) {
+        Write-Host "- - - - - - Output - - - - - -"
+        $keysSorted = $result.Keys | Sort-Object
+        foreach ($key in $keysSorted) {
             if ($result[$key].Count -gt 0) {
                 $uniqe = $result[$key] | Select-Object -Unique | Sort-Object
-                Write-Host "- - - - - - - - - - - - - - -"
-                Write-Host $key
+                Write-Host "Name:" $key
                 foreach($val in $uniqe) {
                     Write-Host `t$val
                 }
+                Write-Host "- - - - - - - - - - - - - - -"
             }
         }
     } else {
-        Write-Host "Please check the file BrowserReg.json"
+        Write-Host "Please check the file location: BrowserReg.json and UACH.txt"
     }
-
 }
 Get-Matcher
